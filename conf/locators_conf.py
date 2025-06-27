ebb4a36ea06bb4bc1999f02537917b479b224d3d
dhruthi <dhruthiysanil123@gmail.com>
2025-06-27 16:20:21 +0530

    commit

--- conf/locators_conf.py
+++ conf/locators_conf.py
@@ -1,10 +1,14 @@
-# Login Page
-magento_email = "//input[@id='email']"
-magento_password = "//input[@id='pass']"
-magento_login_button = "//button[@id='send2']"
+# conf/locators_conf.py
+# Common locator file for all locators
 
-# Women section
-magento_women_link = "//a[@href='https://magento.softwaretestingboard.com/women.html']"
 
-# Search
-magento_search_input = "//input[@id='search']"
+# Magento Login Page Locators
+sign_in_link = "xpath,//a[normalize-space(text())='Sign In']"
+email_field = "xpath,//input[@id='email']"
+password_field = "xpath,//input[@id='pass']"
+sign_in_button = "xpath,//button[@name='send']"  # ✅ Changed from id='send2' to name='send'
+women_section = "xpath,//span[text()='Women']"
+account_url_fragment = "magento.softwaretestingboard.com"
+search_input = "xpath,//input[@id='search']"
+radiant_tee_link = "xpath,//a[contains(@class,'product-item-link') and contains(text(),'Radiant Tee')]"
+
