4b4cf05f8a4dad480d1d958c7259bdd57fe8f6c6
dhruthiysanil <157962100+dhruthiysanil@users.noreply.github.com>
2025-07-03 19:33:08 +0530

    Delete conf/magento_conf.py

--- conf/magento_conf.py
+++
@@ -1,5 +0,0 @@
-class MagentoConf:
-    def __init__(self):
-        self.base_url = "https://magento.softwaretestingboard.com/"
-        self.login_url = self.base_url + "customer/account/login/"
-        self.women_url = self.base_url + "women.html"
